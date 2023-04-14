ca81ddbd4326c2496ae430b68f623191ce7a3d11
Smile-Benjamin <Smile.Benjamin@vodacom.cd>
2023-04-14 19:04:07 +0100

    test get all employees

--- src/test/java/com/bkafirongo/springboottesting/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/bkafirongo/springboottesting/repository/EmployeeRepositoryTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -14,8 +16,6 @@ public class EmployeeRepositoryTests {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-
-//    Junit for save employee operation
 
     /**
      * @bdd covention give_when_then()
@@ -27,15 +27,45 @@ public class EmployeeRepositoryTests {
     @DisplayName("Junit test for save employee operation")
     @Test
     void giveEmployeeObject_whenSave_thenReturnSavedEmployee() {
+
+        //give
         Employee employee = Employee
                 .builder()
                 .firstName("John")
                 .lastName("Doe")
                 .email("email@example.com")
                 .build();
+        //when
         Employee savedEmployee = employeeRepository.save(employee);
+        //then
         assertThat(savedEmployee).isNotNull();
         assertThat(savedEmployee.getId()).isPositive();
+    }
+
+    @DisplayName("Junit test for get all employees list")
+    @Test
+    void givenEmployeeList_whenFindAll_thenReturnEmployeeList() {
+        // give - precondition or setup
+        Employee employee1 = Employee
+                .builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("email@example.com")
+                .build();
+        Employee employee2 = Employee
+                .builder()
+                .firstName("Jane")
+                .lastName("Doe")
+                .email("jane@example.com")
+                .build();
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+        // when - condition or the behaviour that we are going to test
+        List<Employee> employeeList = employeeRepository.findAll();
+        // then - verify the output
+        assertThat(employeeList)
+                .isNotNull()
+                .hasSize(2);
     }
 
 
